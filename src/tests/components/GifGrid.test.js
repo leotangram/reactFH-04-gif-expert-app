404692062f5d401ffd174b0857951db20240a902
Leonardo Omaña <leotangram@gmail.com>
2020-07-15 15:33:29 -0500

    Pruebas del componente gifGrid

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import GifGrid from '../../components/GifGrid'
+import { useFetchGifs } from '../../hooks/useFetchGifs'
+jest.mock('../../hooks/useFetchGifs')
+
+describe('GifGrid Component', () => {
+  const category = 'One Punch'
+  test('should show correctly', () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: false
+    })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('should show items when images is loaded useFetchGif', () => {
+    const gifs = [
+      {
+        id: 'ABC',
+        url: 'hhtps://local.com',
+        title: 'Potato'
+      }
+    ]
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false
+    })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
